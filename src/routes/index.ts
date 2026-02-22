fb91f5a2e45ef7d0100902068061ee8fab7e5000
Carlos Andre <contato.carlosdede@gmail.com>
2026-02-22 02:02:47 -0300

    feat(tables): create tables database schema and list route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { productsRouters } from "./productsRoutes";
+import { tablesRoutes } from "./tablesRoutes";
 
 const routes = Router();
 
 routes.use("/products", productsRouters);
+routes.use("/tables", tablesRoutes);
 
 export { routes };
---
+++ src/routes/tablesRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { TablesController } from "@/controllers/tablesController";
+
+const tablesRoutes = Router();
+const tablesController = new TablesController();
+
+tablesRoutes.get("/", tablesController.index);
+tablesRoutes.post("/", tablesController.create);
+tablesRoutes.put("/:id", tablesController.update);
+tablesRoutes.delete("/:id", tablesController.remove);
+
+export { tablesRoutes };
